d02b15bcbbafd3d2dc5efaa9b1c7704cb97f7a63
Kylie Auld <kylie@unity3d.com>
2019-09-06 13:57:31 -0700

    Bump version: 0.1.0 → 0.2.0

--- bumpv/__init__.py
+++ bumpv/__init__.py
@@ -5,7 +5,7 @@ from .client import BumpClient, exceptions
 from .client.config import Configuration
 from .client.versioning import Version
 
-__VERSION__ = '0.1.0'
+__VERSION__ = '0.2.0'
 
 py_ver = sys.version.split('\n')[0].split(' ')[0]
 DESCRIPTION = f"bumpv: v{__VERSION__} (using Python v{py_ver})"
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = re.sub(
 
 setup(
     name='bumpv',
-    version='0.1.0',
+    version='0.2.0',
     url='https://github.com/kylie-a/bumpv',
     author='Kylie Auld',
     author_email='kylie.a@protonmail.com',
