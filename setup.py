f9c7bfba9d2d1ee49c07c8a560dc97c07ff5b701
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2019-11-24 17:10:40 +0100

    Migrate package metadata from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,37 +1,3 @@
-import re
-from setuptools import setup, find_packages
+from setuptools import setup
 
-description = 'Version-bump your software with a single command!'
-
-with open('README.rst') as handle:
-    long_description = handle.read()
-
-setup(
-    name='bumpv',
-    version='0.3.0',
-    url='https://github.com/kylie-a/bumpv',
-    author='Kylie Auld',
-    author_email='kylie.a@protonmail.com',
-    license='MIT',
-    packages=find_packages(),
-    description=description,
-    long_description=long_description,
-    install_requires=[
-        "Click==7.0",
-        "pyaml==19.4.1",
-    ],
-    entry_points={
-        'console_scripts': [
-            'bumpv = bumpv:bumpv',
-        ]
-    },
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: PyPy',
-    ],
-)
+setup()
